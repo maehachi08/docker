060b372654049baee471ed4aa66c36f966573140
maehachi08 <maehachi08@gmail.com>
2016-08-09 22:27:26 +0900

    add docker build script for go-cd

---
+++ build/JavaServletHelloWorld/scripts/go-cd/docker-build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# GO CDでDockerマシンイメージをビルドするためのスクリプト
+# 
+#  本スクリプトはGO CDエージェント上で実行すること想定する
+#  ディレクトリ構成は以下を想定。
+#    基点:
+#      /var/lib/go-agent/pipelines/JavaServletHelloWorld
+#
+#    gitレポジトリ配布: 
+#      /var/lib/go-agent/pipelines/JavaServletHelloWorld/git
+#
+
+# dockerビルド素材をコピー
+mkdir -p work/docker/hello
+cp -a git/docker/build/JavaServletHelloWorld/ work/docker/
+
+# Javaアプリをコピー
+cp -a git/JavaServletHelloWorld/WEB-INF work/docker/hello/
+
+# docker build
+docker build -t maehachi08/java_servlet_hello_world work/docker
+
