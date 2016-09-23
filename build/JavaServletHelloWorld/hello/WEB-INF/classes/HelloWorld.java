fa5c54d7c3d882112498f1facad4766109ae08c1
maehachi08 <maehachi08@gmail.com>
2016-09-23 08:54:28 +0000

    処理毎にMath.random関数を10000回実行させ、no-cacheメタパラメータを付加して明示的にキャッシュさせない

--- build/JavaServletHelloWorld/hello/WEB-INF/classes/HelloWorld.java
+++ build/JavaServletHelloWorld/hello/WEB-INF/classes/HelloWorld.java
@@ -10,9 +10,30 @@ public class HelloWorld extends HttpServlet {
 
   throws IOException, ServletException
   {
+
+    /**
+     *  Math.random関数で負荷をかける
+     */
+    int i = 0;
+    double rand = 0;
+    while (i < 10000) {
+      rand += Math.random();
+      i++;
+    }
+
+    /**
+     * HelloWorldを返す
+     */
     PrintWriter out = response.getWriter();
+    out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">");
     out.println("<html>");
+    out.println("<head>");
+    out.println("<meta http-equiv=\"Pragma\" content=\"no-cache\"> <meta http-equiv=\"Cache-Control\" content=\"no-cache\"> <meta http-equiv=\"Expires\" content=\"0\">");
+    out.println("<title>HelloWorld</title>");
+    out.println("</head>");
+    out.println("<body>");
     out.println("HelloWorld");
+    out.println("</body>");
     out.println("</html>");
     out.close();
   }
